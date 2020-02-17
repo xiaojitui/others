55392330c6dab8c20a8800c81072be8339003ebf
xiaojitui <tomking576@gmail.com>
2020-02-16 22:58:43 -0500

    Create translator.py

---
+++ translator.py
@@ -0,0 +1,9 @@
+from googletrans import Translator
+translator = Translator()
+print(translator.translate('もしもし'))
+## output: Translated(src=ja, dest=en, text=Hello, pronunciation=None, extra_data="{'translat...")
+
+
+print(translator.translate(text))
+
+
